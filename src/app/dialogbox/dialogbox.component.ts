3e719f80fba9e0b067a6cfe56f4df6c24ac94be4
forkanselise <thouhidur.forkan@selise.ch>
2023-05-08 11:40:54 +0600

    image uploading and thumbnail added

--- src/app/dialogbox/dialogbox.component.ts
+++ src/app/dialogbox/dialogbox.component.ts
@@ -13,7 +13,8 @@ export class DialogboxComponent implements OnInit {
   videoInfo = this.fb.group({
     Title: [''],
     VideoUrl: [''],
-    Description: ['']
+    Description: [''],
+    Thumbnail: ['']
   });
 
   constructor(
@@ -23,6 +24,41 @@ export class DialogboxComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+
+  public picked(event:any) {
+    // this.currentId = field;
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      const file: File = fileList[0];
+      // this.sellersPermitFile = file;
+      this.handleInputChange(file); //turn into base64
+    }
+  }
+
+
+  handleInputChange(files:any) {
+    var file = files;
+    var pattern = /image-*/;
+    var reader = new FileReader();
+    if (!file.type.match(pattern)) {
+      alert('invalid format');
+      return;
+    }
+    reader.onloadend = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+  _handleReaderLoaded(e:any) {
+    let reader = e.target;
+    var base64result = reader.result.substr(reader.result.indexOf(',') + 1);
+    //this.imageSrc = base64result;
+    // let id = this.currentId;
+    console.log(base64result);
+
+    this.videoInfo.get('Thumbnail')?.setValue(base64result);
+
+    // this.log();
   }
 
   saveData() {
